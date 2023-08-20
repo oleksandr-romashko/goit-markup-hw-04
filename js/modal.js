f9b4e08117f1efa28d2eca7c508b0747a610820e
Oleksandr Romashko <alex.rmshk@gmail.com>
2023-08-20 17:22:51 +0200

    Add comments to the modal window script

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,9 @@
 // This script shows and hides modal window
+// Set following atributes:
+// data-modal-open - on the button to open the modal window.
+// data-modal-close - on the button to close the modal window.
+//data-modal - on the modal window backdrop.
+
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
